ca7cbfaeb90415dd5b355e9b3dea538d0fe94b2c
Valero <fernando.valero@mkcorp.com>
2017-08-24 19:06:55 -0500

    1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,41 @@
-import { Component } from '@angular/core';
-
-export class Hero{
-	id:number;
+import { Component,OnInit } from '@angular/core';
+import { MenusService } from "./menus.service";
+export class Menu{
+	id:string;
 	name:string;
 }
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers:[MenusService]
 })
 
 
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'e-Commerce';
-  hero: Hero = {
-  	id:1,
-  	name:'Batman'
-  };
+  menuJSON: any;
+  menuActual: Menu[];
+
+
+  constructor(private menusService:MenusService){}
+
+  ngOnInit(){
+    this.menuActual=[{id:"",name:""}];
+    this.getMenus();
+  }
+  getMenus(){
+    this.menuActual.splice(0);
+    this.menusService.getMenuAll().then(r=>{
+      var menu = r;
+      menu.submenu.forEach(element => {
+        if(element.parent=="root"){
+          this.menuActual.push({"id":element.id,name:element.displayname});
+        }
+      });
+      
+    });
+    
+  }
 }
---
+++ src/app/menus.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { Headers, Http } from "@angular/http";
+import 'rxjs/add/operator/toPromise';
+
+@Injectable()
+export class MenusService{
+
+  constructor(private http:Http) { }
+
+  getMenuAll():Promise<any>{
+    return this.http.get("http://localhost:3000/menus")
+                    .toPromise()
+                    .then(r=>JSON.parse(r["_body"])[0])
+                    .catch(this.ErrorHandler);
+  }
+
+  ErrorHandler(error: any):Promise<any>{
+    console.log("Error has occurred",error);
+    return Promise.reject(error.message);
+  }
+  
+}
