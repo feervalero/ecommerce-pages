c7023a3c522dffd9b0701c636b1db1a6fe57a2f6
Fer Valero <feervalero@gmail.com>
2017-09-10 12:23:13 -0500

    1223

--- src/app/menus.service.ts
+++ src/app/menus.service.ts
@@ -15,35 +15,7 @@ export class MenusService{
   }
 
   getMenus(): Promise<any>{
-    var xmlDoc;
-    //load xml
-    var parser = new DOMParser();
     return this.http.request("assets/CATALOGXML.xml").toPromise().then(r=>r["_body"]);
-      /*
-      xmlDoc = parser.parseFromString(x["_body"],"text/xml");
-      var mcs = xmlDoc.getElementsByTagName("menuItem");
-      var menuBar = document.getElementById("menuBar");
-      menuBar.innerHTML="";
-      for (var i = 0; i < mcs.length; i++) {
-        if(mcs[i].attributes['parent'].value.toString()=="root"){
-          var span = document.createElement("a");
-          var li = document.createElement("li");
-          
-          span.setAttribute("parent",mcs[i].attributes["parent"].value);
-          span.setAttribute("id",mcs[i].attributes["id"].value);
-          span.setAttribute("collapsed","1");
-          span.setAttribute("href","/catalog-list/"+mcs[i].attributes["id"].value);
-          span.innerHTML=mcs[i].attributes["displayName"].value.toString();
-          li.appendChild(span);
-  
-          li.setAttribute("parent",mcs[i].attributes["parent"].value);
-          li.setAttribute("id",mcs[i].attributes["id"].value);
-          li.setAttribute("collapsed","1");
-          menuBar.appendChild(li);
-        }
-      }
-    });*/
-    
   }
 
   ErrorHandler(error: any):Promise<any>{
